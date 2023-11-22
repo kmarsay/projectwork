63a621eb5acdc7393bae869b5ca0c425efd6c4be
Marsay K <m.k@masterfield.com>
2023-11-22 15:32:45 +0100

    11.22. próbálkozások

--- src/test/java/hu/masterfield/pages/HomePage.java
+++ src/test/java/hu/masterfield/pages/HomePage.java
@@ -1,4 +1,57 @@
 package hu.masterfield.pages;
 
-public class HomePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.concurrent.locks.Condition;
+
+public class HomePage extends BasePage {
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    //Nem lesz külön LanguageChange page
+
+    //cookies Button  - jobb xpath kellene még!
+    //    //*[@id="sticky-bar-cookie-wrapper"]/span/div/div/div[2]/form[1]/button/span
+    @FindBy(xpath = "//*[@id=\"sticky-bar-cookie-wrapper\"]/span/div/div/div[2]/form[1]/button")
+    WebElement acceptCookiesButton;
+
+    //language Button
+    //xpath lehetne jobb megoldás, mint ez? //*[@id="utility-header-language-switch-link"]/span/text()
+    @FindBy(xpath = "//*[@id=\"utility-header-language-switch-link\"]/span/text()")
+    WebElement langButton;
+
+
+    public void acceptCookies() {
+        acceptCookiesButton.click();
+    }
+
+    public boolean isLoaded() {
+        return isLoaded(acceptCookiesButton);
+
+    }
+
+    public void changeLang(String language) {
+        if ((langButton.getText().equals("Magyar") &&
+                language.equals("magyar")) ||
+                (langButton.getText().equals("English") &&
+                        language.equals("angol"))) {
+            langButton.click();
+        }
+
+        }
+
+    // public void validationLang(String language){}, át tudtam váltani angolra
+
+    //Lang Button ist vorhanden:
+    //esetleg kellett volna még egy Step, ami a lang buttont keresi?
+   // public void checkLangButton(){
+     //   langButton.shouldBe(Condition.visible).shouldBe(Condition.enabled);
+   // }
+
+    }
+
 }
--- src/test/java/hu/masterfield/steps/TescoSteps.java
+++ src/test/java/hu/masterfield/steps/TescoSteps.java
@@ -1,48 +1,86 @@
 package hu.masterfield.steps;
 
+import hu.masterfield.pages.BasePage;
+import hu.masterfield.pages.HomePage;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.InterruptedIOException;
+import java.time.Duration;
 
 public class TescoSteps {
 
-    //@Before                Chrome nyit
+    BasePage basePage;
+    HomePage homePage;
+
+    @BeforeAll
 
 
+    public static void setup() throws InterruptedIOException {
 
-    //BAckground-ként valami annotációval (main page open and accept cookies:GIVEN, AND) vagy maradjon így?
+
+        // set chrome options
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
+
+        // init driver
+        ChromeDriver driver = new ChromeDriver(chromeOptions);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        //window size
+        driver.manage().window().setSize(new Dimension(900, 900));
+    }
+
+
+    @AfterAll
+    //böngésző zárása
+    public static void cleanup() {
+        WebDriver driver;
+        driver.quit();
+    }
 
     @Given("main page is open")
     public void mainPageIsOpen() {
+        WebDriver driver;
+        driver.get("https://bevasarlas.tesco.hu/groceries/hu-HU");
+        homePage = new HomePage(driver);
+        homePage.isLoaded();
     }
 
-
-    @And("I click on accept cookies")
-    public void iClickOnAcceptCookies() {
+    @And("accept cookies")
+    public void acceptCookies() {
+        homePage.acceptCookies();
     }
 
-    //Background vége lenne
-
-    @And("language is set to {Magyar}")
+    @Given("language is set to Magyar")
     public void languageIsSetTo(String Magyar) {
     }
 
+    // change lang. nem végleges, elakadtam
     @When("I change the language to {English}")
-    public void iChangeTheLanguageTo(String English) {
+    public void iChangeTheLanguageTo(String language) {
+        homePage.changeLang(language);
     }
 
-    @Then("it shows elements in {English}")
-    public void itShowsElementsIn(String English) {
-    }
-
-    /*
-    @After
-    public void cleanup() {
-        WebDriverRunner.getWebDriver().quit();
-        */
-    }
-
+    //lang validálással próbálkozok
+    // @Then("it shows elements in English")
+    // public void itShowsElementsIn(String language) {
+    //     language.validation(language);
 }
+
+
+
+
+
+
+
